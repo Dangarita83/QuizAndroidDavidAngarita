bedbf9910a628475e7f8d31a34c6892b3dff1ea3
David Angarita <dangarita83@unab-.edu.co>
2026-02-27 09:31:33 -0500

    UI implementada

--- app/src/main/java/me/davidangarita/quizandroiddavidangarita/MainActivity.kt
+++ app/src/main/java/me/davidangarita/quizandroiddavidangarita/MainActivity.kt
@@ -4,13 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import me.davidangarita.quizandroiddavidangarita.ui.theme.QuizAndroidDavidAngaritaTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,30 +26,97 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            QuizAndroidDavidAngaritaTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            primeraPantalla()
+
+
+
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    QuizAndroidDavidAngaritaTheme {
-        Greeting("Android")
+fun primeraPantalla() {
+    Column(modifier = Modifier
+        .padding(start = 16.dp, end = 16.dp)
+        .fillMaxSize(),
+        verticalArrangement = Arrangement.SpaceAround) {
+
+
+        Row(modifier = Modifier
+            .fillMaxWidth(),
+            horizontalArrangement = Arrangement.Center
+        ){
+            Text(
+                text = "Hello David",
+                modifier = Modifier
+                    .background(Color.Blue)
+                    .padding(10.dp)
+
+            )
+
+        }
+        
+
+
+
+
+        Row(modifier = Modifier
+            .fillMaxWidth()
+            .background(Color.Green)
+            .padding(20.dp),
+            horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+            Text(
+                text = "item 1",
+                modifier = Modifier
+                    .background(Color.Gray)
+                    .padding(10.dp)
+
+            )
+
+            Text(
+                text = "item 2",
+                modifier = Modifier
+                    .background(Color.Gray)
+                    .padding(10.dp)
+
+            )
+            Text(
+                text = "item 3",
+                modifier = Modifier
+                    .background(Color.Gray)
+                    .padding(10.dp)
+
+
+            )
+
+        }
+
+
+
+
+
+
+
+        Row(modifier = Modifier
+            .fillMaxWidth(),
+            horizontalArrangement = Arrangement.Center
+        ){
+            Text(
+                text = "Pie de pagina",
+                modifier = Modifier
+                    .background(Color.LightGray)
+                    .padding(10.dp)
+
+            )
+
+        }
+
+
+
     }
+
+
 }
